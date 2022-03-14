45612415c37339743cecf2bf9169b143fc823ec4
FredZxx <frezxx8641@gmail.com>
2022-03-14 08:46:29 +0800

    1

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+int main() { 
+    int i = 1;
+    printf("%d", i);
+    printf("%d", ++i);
+    printf("hello world\n");
+    printf("zhou xinxin\n");
+    return 0;
+}
+
+
+
+
+
+
+
